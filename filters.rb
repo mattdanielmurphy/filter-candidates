277837a30fe16c63662aaa658f065f3ebf3da29a
Matthew Daniel Murphy <matt@mattmurphy.ca>
2018-01-16 13:58:07 -0500

    completed exercises

--- filters.rb
+++ filters.rb
@@ -1,16 +1,50 @@
-# In this file we define the methods to help filter out candidates
-# This way, we keep these methods separated from other potential parts of the program
+require './candidates'
+
 
 def find(id)
-  # Your code Here
+  @candidates.each do |cand|
+    return cand if cand[:id] == id
+  end
+  nil
 end
 
-def experienced?(candidate)
-  # Your code Here
+def experienced?(cand)
+  return true if cand[:years_of_experience] > 1
+  false
 end
 
-def qualified_candidates(candidates)
-  # Your code Here
+def knows_ruby_or_python?(cand)
+  if ( cand[:languages] & ['Ruby', 'Python'] ).present?
+    return true
+  end
+  false
 end
 
-# More methods will go below
+def applied_recently?(cand)
+  return true if (Date.today - cand[:date_applied]) < 16
+  false
+end
+
+def criteria_met?(cand)
+  if (
+      experienced?(cand) &&
+      cand[:github_points] > 99 &&
+      knows_ruby_or_python?(cand) &&
+      applied_recently?(cand) &&
+      cand[:age] > 17
+    )
+    return true
+  end
+  false
+end
+
+def qualified_candidates(cands)
+  cands.select { |cand| criteria_met?(cand) }
+end
+
+def ordered_by_qualifications(cands)
+  cands.sort_by{ |cand| [
+                          cand[:years_of_experience],
+                          cand[:github_points]
+                        ]}.reverse
+end
